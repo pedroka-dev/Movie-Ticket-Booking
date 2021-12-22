41af00eb10e641972514a16ff40acd105226f1fc
pedro-ca <pedro.henric.rosario@gmail.com>
2021-12-22 10:24:22 -0300

    Added dialogAlert and some minor visual changes

--- MovieTicketBooking/app/src/main/java/br/pedroca/movieticketbooking/TicketListActivity.java
+++ MovieTicketBooking/app/src/main/java/br/pedroca/movieticketbooking/TicketListActivity.java
@@ -1,8 +1,13 @@
 package br.pedroca.movieticketbooking;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.navigation.ui.AppBarConfiguration;
 
@@ -25,4 +30,29 @@ public class TicketListActivity extends Activity {
         List<Ticket> ticketList = new TicketDao().GetAll();
         ticketListView.setAdapter(new TicketListAdapter(ticketList,this));
     }
+
+    public void showAlertDialogButtonClicked(View view)
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("'MovieName' Added to Cart");
+
+        final View customLayout = getLayoutInflater().inflate( R.layout.dialog_alert, null);
+        builder.setView(customLayout);
+
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //EditText editText= customLayout.findViewById(R.id.editText);
+                //sendDialogDataToActivity(ditText.getText().toString());
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    //private void sendDialogDataToActivity(String data)
+    //{
+    //    Toast.makeText(this,data,Toast.LENGTH_SHORT).show();
+    //}
 }
